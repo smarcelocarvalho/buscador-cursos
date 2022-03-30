d9f161be113db637b679e109477b26018ad24008
Marcelo Carvalho <marcelo_scarvalho@outlook.com>
2022-03-30 19:45:25 -0300

    Resolvido status de erro

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client();
+$client = new Client(['verify' => false]);
 $resposta = $client->request('GET','https://cursos.alura.com.br/category/programacao/php');
 
 $html = $resposta->getBody();
@@ -13,7 +13,7 @@ $html = $resposta->getBody();
 $crawler = new Crawler();
 $crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.course-card_name');
+$cursos = $crawler->filter('span.course-card__name');
 
 foreach ($cursos as $curso):
     echo $curso->textContent . PHP_EOL;
